ad9500b10482caea4d7851f0e0ed4afd981a453f
fuser <sahil.dogstar@gmail.com>
2019-10-01 12:50:04 +0530

    Completed First Exercise, Day 2.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,48 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+
+  Container upperSection = new Container(
+    height: 100,
+    color: Colors.yellow,
+  );
+  Container lowerSection = new Container(
+    height: 100,
+    color: Colors.green,
+  );
+
+  Container leftSection = new Container(
+    width : 100,
+    color: Colors.red,
+  );
+  Container middleSection = new Container(
+    width: 100,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[upperSection, lowerSection],
+    ),
+  );
+  Container rightSection = new Container(
+    width: 100,
+    color: Colors.blue,
+  );
+
+
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(),
+        backgroundColor: Colors.purple,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              leftSection,
+              middleSection,
+              rightSection
+            ],
+          ),
+        ),
       ),
     ),
   );
